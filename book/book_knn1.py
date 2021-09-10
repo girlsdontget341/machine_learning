41550468efba352cd9bd81732c852b5d926f0164
ssy <1030684663@qq.com>
2021-09-11 00:34:22 +0800

    约会预测

--- book/book_knn1.py
+++ book/book_knn1.py
@@ -4,26 +4,26 @@ group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
 labels = ['A', 'A', 'B', 'B']
 def classify0(inX, dataset, labels, k):
     datasetsize= dataset.shape[0]
-    print(datasetsize)
+    # print(datasetsize)
     diffmat = tile(inX, (datasetsize, 1)) - dataset
     sqdiffmat = diffmat**2
     sqdistances = sqdiffmat.sum(axis=1)
     distances= sqdistances**0.5
     sorteddisindices= distances.argsort()#从小到大返回索引值
-    print(sorteddisindices)
+    # print(sorteddisindices)
     classcount ={}
     for i in range(k):
         voteilabel = labels[sorteddisindices[i]]
         #print(voteilabel)
         classcount[voteilabel]= classcount.get(voteilabel, 0) + 1
-        print(classcount[voteilabel])
+        # print(classcount[voteilabel])
     sortedclasscount =sorted(classcount.items(), key=operator.itemgetter(1), reverse=True)
     #.items()将字典变对应元组列表
     #itemgetter定义函数，获取对象的第1个域的值
     #reverse降序
     #获得最近3个哪类出现的次数多 即属于哪类
-    print(sortedclasscount[0][0])
+    #print(sortedclasscount[0][0])
     return sortedclasscount[0][0]
-if __name__ == '__main__':
-    classify0([0,0], group, labels, 3)
+# if __name__ == '__main__':
+#     classify0([0,0], group, labels, 3)
 
--- book/knn_Appointment.py
+++ book/knn_Appointment.py
@@ -1,6 +1,7 @@
 from numpy import *
-import matplotlib.pyplot as plt
-def file2matrix(filename):
+import book_knn1
+#import matplotlib.pyplot as plt
+def file2matrix(filename):#文本转化为numpy
     fr = open(filename)
     arrayolines = fr.readlines() #读取文件行
     numberoflines = len(arrayolines) #获取行数
@@ -21,9 +22,49 @@ def file2matrix(filename):
         classlabelvector.append(int(listfromline[-1]))
         index += 1
     return returnmat, classlabelvector
-if __name__ == '__main__':
+
+def autonorm(dataset):
+    minval = dataset.min(0)
+    maxval = dataset.max(0)
+    range = maxval - minval
+    normdataset = zeros(shape(dataset))
+    m = dataset.shape[0]
+    normdataset = dataset - tile(minval,(m, 1) )#tile重复函数 即将minval复制1行m次
+    normdataset2 = normdataset / tile(range, (m, 1))
+    return normdataset2, range, minval
+
+def datingtest():#只使用10%数据作为测试数据
+    test_per = 0.10
     datingdatamat, datinglabels = file2matrix('datingTestSet2.txt')
-    fig = plt.figure()
-    ax = fig.add_subplot(111) #用法见https://blog.csdn.net/qq_43411749/article/details/106864723?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link
-    ax.scatter(datingdatamat[:,1], datingdatamat[:,2], )#scatter传递坐标
-    plt.show()
+    normmat, ranges, minval = autonorm(datingdatamat)
+    m = normmat.shape[0]
+    error= 0.0
+    numberoftest = int(m*test_per)
+    for i in range(numberoftest):
+        classresult = book_knn1.classify0(normmat[i, :], normmat[numberoftest:m, :], datinglabels[numberoftest:m], 3)
+        print("分类器返回预测结果为：%d ，真实结果为： %d"%(classresult, datinglabels[i]))
+        if(classresult!=datinglabels[i]):
+            error += 1.0
+            print("本次预测失败..")
+        else: print("本次预测成功！")
+    print("本次使用错误率为 %f" % (error / float(numberoftest)))
+
+def classifyperson():#约会喜欢程度预测函数
+    resultlist = ["你就是个粑粑，爬！", "一般般吧，但还不配", "我要给你生猴子！"]
+    per2 = float(input("你一周花（）%的时间玩游戏？"))
+    per1 = float(input("你每年有（）公里数旅游？"))
+    per3 = float(input("你每周有（）公升冰淇淋消费？"))
+    arrtest = array([per1, per2, per3])
+    datingdatamat, datinglabels = file2matrix('datingTestSet2.txt')
+    normmat, ranges, minval = autonorm(datingdatamat)
+    result = book_knn1.classify0((arrtest-minval)/ranges, normmat, datinglabels, 3)
+    print("你获得的结果是：", resultlist[result-1])
+
+if __name__ == '__main__':
+    # datingtest()
+    #画散点图
+    # fig = plt.figure()
+    # ax = fig.add_subplot(111) #用法见https://blog.csdn.net/qq_43411749/article/details/106864723?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link
+    # ax.scatter(datingdatamat[:,1], datingdatamat[:,2], )#scatter传递坐标
+    # plt.show()
+    classifyperson()
