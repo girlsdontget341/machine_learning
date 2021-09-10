dde1b59840de24b3fc9c593a1c293f8dadd7ec34
ssy <1030684663@qq.com>
2021-09-10 15:24:20 +0800

    转换文本记录

--- book/knn_Appointment.py
+++ book/knn_Appointment.py
@@ -1,9 +1,29 @@
 from numpy import *
-
+import matplotlib.pyplot as plt
 def file2matrix(filename):
     fr = open(filename)
-    array0lines = fr.readlines()
-    numberoflines = len(array0lines)
+    arrayolines = fr.readlines() #读取文件行
+    numberoflines = len(arrayolines) #获取行数
     returnmat = zeros((numberoflines,3))
+     #生成[[0. 0. 0.]
+     # [0. 0. 0.]
+     # [0. 0. 0.]
+     # ...
+     # [0. 0. 0.]
+     # [0. 0. 0.]
+     # [0. 0. 0.]]  1000行
     classlabelvector = []
     index = 0
+    for line in arrayolines:
+        line = line.strip()#移除首尾空格
+        listfromline = line.split('\t') #按制表符分割 返回列表
+        returnmat[index, :] = listfromline[0:3]
+        classlabelvector.append(int(listfromline[-1]))
+        index += 1
+    return returnmat, classlabelvector
+if __name__ == '__main__':
+    datingdatamat, datinglabels = file2matrix('datingTestSet2.txt')
+    fig = plt.figure()
+    ax = fig.add_subplot(111) #用法见https://blog.csdn.net/qq_43411749/article/details/106864723?utm_medium=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link&depth_1-utm_source=distribute.pc_relevant.none-task-blog-2%7Edefault%7ECTRLIST%7Edefault-1.no_search_link
+    ax.scatter(datingdatamat[:,1], datingdatamat[:,2], )#scatter传递坐标
+    plt.show()
