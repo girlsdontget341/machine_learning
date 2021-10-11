5549ff078b1bc21363d98dbddafe6bc3d51703f2
ssy <1030684663@qq.com>
2021-10-11 23:04:04 +0800

    天池XGBoost

--- book/k_means/k_means.py
+++ book/k_means/k_means.py
@@ -1,5 +1,5 @@
 from numpy import *
-
+import matplotlib.pyplot as plt
 def loadDataset(filename):
     dataMat = []
     fr = open(filename)
@@ -7,7 +7,7 @@ def loadDataset(filename):
         curline = line.strip().split('\t')
         fitline = list(map(float, curline)) #map() 会根据提供的函数对指定序列做映射。(对每个curline取float值） Python3 返回迭代器需转换为list
         dataMat.append(fitline)
-    return mat(dataMat)
+    return array(dataMat)
 
 def calculate_Eclud(vecA, vecB):#计算两向量之间的欧氏距离
     return sqrt(sum(power(vecA-vecB, 2)))
@@ -45,14 +45,35 @@ def k_means(dataSet,k):
             centroids[cent,:] = mean(ptsinCent, axis=0)
     return centroids, cluster
 
+
+
+def plot_scatter(clusters, centroids):
+    idx0 = where(clusters[:, 0] == 0)
+    idx1 = where(clusters[:, 0] == 1)
+    idx2 = where(clusters[:, 0] == 2)
+    idx3 = where(clusters[:, 0] == 3)
+    #where函数返回的是两个数组 第一个横坐标 第二个纵坐标
+    #此处画图只需取横坐标
+    plt.scatter(x[idx0[0], 0].tolist(), x[idx0[0], 1].tolist(), marker='x', color='r')
+    plt.scatter(x[idx1[0], 0].tolist(), x[idx1[0], 1].tolist(), marker='x', color='g')
+    plt.scatter(x[idx2[0], 0].tolist(), x[idx2[0], 1].tolist(), marker='x', color='y')
+    plt.scatter(x[idx3[0], 0].tolist(), x[idx3[0], 1].tolist(), marker='x', color='b')
+    plt.scatter(centroids[:, 0].tolist(), centroids[:, 1].tolist(), marker='o', color='black', linewidths=10)
+    plt.xlabel('X1')
+    plt.ylabel('X2')
+    plt.show()
+
 if __name__ == '__main__':
-    data = mat(loadDataset('testSet.txt'))
+    x = (loadDataset('testSet.txt'))
+
     # print(data)
     # centroid = rand_Centroid(data,4)
     # print(centroid)
     # re = calculate_Eclud(centroid[0],centroid[1])
     # print(re)
-    centroids, clusters = k_means(data, 4)
+    centroids, clusters = k_means(x, 4)
     print(centroids)
     print("------------------")
     print(clusters)
+    plot_scatter(clusters, centroids)
+
--- 天池/XGBoost.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-import pandas as pd
-import matplotlib.pyplt as plt
-
-
-
-
